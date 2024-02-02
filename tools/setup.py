7801f58af44f55acd2390c69280c0ad8be583405
Jungi KIM <jungi.kim@systrangroup.com>
2024-02-02 11:04:24 -0800

    helper script for CI

---
+++ tools/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# coding: UTF-8
+
+from setuptools import setup
+
+setup(
+    name='dummy',
+    version='0.0.1',
+    description='dummy package',
+    packages=['dummy'],
+)
