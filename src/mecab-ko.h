a5182bf7053bea7daee091a38ab4bf484cf11ae6
Jungi KIM <jungi.kim@systrangroup.com>
2024-02-02 18:47:34 -0800

    try fixing compilation on windows (https://github.com/microsoft/cppwinrt/issues/479)

--- src/mecab-ko.h
+++ src/mecab-ko.h
@@ -336,6 +336,9 @@ extern "C" {
 #endif
 
 #ifdef _WIN32
+#ifndef NOMINMAX
+#define NOMINMAX
+#endif
 #include <windows.h>
 #  ifdef DLL_EXPORT
 #    define MECAB_KO_DLL_EXTERN  __declspec(dllexport)
